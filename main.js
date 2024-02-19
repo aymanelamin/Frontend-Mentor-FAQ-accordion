ae35c65c20c06166ac42a9aab0086187597b364e
aymanelamin <aymanelamin06@gmail.com>
2024-02-19 08:18:37 +0200

    first submmit for FAQ accordion

---
+++ main.js
@@ -0,0 +1,49 @@
+
+function plus1() {
+    document.getElementById('first-answer').style.display = "block";
+    document.getElementById('plus-icon1').style.display = "none";
+    document.getElementById('minus-icon1').style.display = "block";
+}
+
+function minus1() {
+    document.getElementById('first-answer').style.display = "none";
+    document.getElementById('plus-icon1').style.display = "block";
+    document.getElementById('minus-icon1').style.display = "none";
+}
+
+function plus2() {
+    document.getElementById('second-answer').style.display = "block";
+    document.getElementById('plus-icon2').style.display = "none";
+    document.getElementById('minus-icon2').style.display = "block";
+}
+
+function minus2() {
+    document.getElementById('second-answer').style.display = "none";
+    document.getElementById('plus-icon2').style.display = "block";
+    document.getElementById('minus-icon2').style.display = "none";
+}
+
+function plus3() {
+    document.getElementById('third-answer').style.display = "block";
+    document.getElementById('plus-icon3').style.display = "none";
+    document.getElementById('minus-icon3').style.display = "block";
+}
+
+function minus3() {
+    document.getElementById('third-answer').style.display = "none";
+    document.getElementById('plus-icon3').style.display = "block";
+    document.getElementById('minus-icon3').style.display = "none";
+}
+
+function plus4() {
+    document.getElementById('forth-answer').style.display = "block";
+    document.getElementById('plus-icon4').style.display = "none";
+    document.getElementById('minus-icon4').style.display = "block";
+}
+
+function minus4() {
+    document.getElementById('forth-answer').style.display = "none";
+    document.getElementById('plus-icon4').style.display = "block";
+    document.getElementById('minus-icon4').style.display = "none";
+}
+
